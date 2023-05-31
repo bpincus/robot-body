c7a8ba712dfabea007c4d38da48c7b230681b7fd
Benjamin Pincus <bpincus@vmware.com>
2023-05-31 13:39:50 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    pathMatch: 'full',
+  },
+  // Your route here:
+  { 
+    path: 'brain', loadChildren: () => import('mfe1/Module').then(m => m.BrainModule)
+  },
+  {
+    path: 'brain', loadChildren: () => loadRemoteModule({
+      type: 'manifest',
+      remoteName: 'mfe1',
+      exposedModule: './Module'
+    }).then(m => m.BrainModule)
+  },
+  {
+    path: '**',
+    component: AppComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
